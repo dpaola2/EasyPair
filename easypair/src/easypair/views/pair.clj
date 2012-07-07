61c0da4317f4ec919bf3a7fce4e387d720568282
Dave Paola <dpaola2@gmail.com>
2012-07-06 21:09:59 -0700

    session data

--- easypair/src/easypair/views/pair.clj
+++ easypair/src/easypair/views/pair.clj
@@ -15,7 +15,7 @@
    (ref-set sessions (assoc (deref sessions) ipaddr []))))
 
 (defn list-sessions [] 
-  (println (keys (deref sessions))))
+  (keys (deref sessions)))
 
 (defn keystroke [ipaddr contents]
   (println 
@@ -27,4 +27,15 @@
       ", " 
       (get (deref sessions) ipaddr)))))
 
-(println list-sessions)
+(defn generate-session-data []
+  (do
+    (new-session "192.168.1.1")
+    (join "192.168.1.1" "1.1.1.1")
+    (join "192.168.1.1" "2.2.2.2")
+    (join "192.168.1.1" "3.3.3.3")
+    (new-session "10.5.5.5")
+    (join "10.5.5.5" "4.4.4.4")
+    (join "10.5.5.5" "5.5.5.5")
+    (join "10.5.5.5" "6.6.6.6")))
+
+(generate-session-data)
