7105ccc38bf25a356d053faa1382c695021c9c5e
Dave Paola <dpaola2@gmail.com>
2012-07-15 11:21:28 -0700

    client

--- client/app.js
+++ client/app.js
@@ -9,9 +9,24 @@ window.code_editor = function() {
         }
         $('.debug').html(JSON.stringify(output)); //change this to POST to a url
     }
-    this.editor.getSession().on('change', _.debounce(this.broadcast, 10));
+    //this.editor.getSession().on('change', _.debounce(this.broadcast, 10));
+}
+
+window.poll_session = function(session_hash) {
+    $.ajax({
+        url: "http://localhost:8080/session/" + session_hash,
+        success: function(data, status, xhr) {
+            var contents = data;
+            window.editor.getSession().setValue(contents);
+        },
+        error: function(data, status, xhr) {alert("something went wrong");}
+    });
 }
 
 $(function() {
-      window.code_editor();
+    window.code_editor();
+    $('#connect-btn').on('click', function() {
+        var session_hash = $('#session-hash').val();
+        window.poll_session(session_hash);
+    })
 });
