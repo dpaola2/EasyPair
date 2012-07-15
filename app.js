5d83cc2f7dfde1aad4a04fe9982d621b166a0a3d
Dave Paola <dpaola2@gmail.com>
2012-07-15 12:57:54 -0700

    node first pass

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.use("/client", express.static(__dirname));
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
